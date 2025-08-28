a2e18d3c474fa63eed0d044b4f5f74e9ca5ad7b8
arinleviti <arin.leviti@gmail.com>
2025-08-28 18:10:18 +0200

    videos on cloudify

--- client/src/app/_services/projects.ts
+++ client/src/app/_services/projects.ts
@@ -10,14 +10,14 @@ export const projects: Project[] = [
   {
     title: 'Hero Burger',
     description: 'Responsive React and Next.js website for a restaurant in Predazzo, Italy. Includes Framer Motion animations, dynamic menu filtering, a Supabase-powered blog with CRUD, and a Swiper carousel for featured items.',
-    videoUrl: '/HeroBurger.mp4',
+    videoUrl: 'https://res.cloudinary.com/doydy0awd/video/upload/f_auto,q_auto,fl_lossy,h_720,vc_auto/v1756396707/HeroBurger_mcpkxj.mp4',
     posterUrl: '/HeroBurgerPoster.webp',
     linkUrl: 'https://herolacasadelburger.it'
   },
   {
     title: 'Restaurant template',
     description: 'A customizable restaurant website template built with React.',
-    videoUrl: '/BestBurgers.mp4',
+    videoUrl: 'https://res.cloudinary.com/doydy0awd/video/upload/f_auto,q_auto,fl_lossy,h_720,vc_auto/v1756396722/BestBurgers_jpjyit.mp4',
     posterUrl: '/BestBurgersPoster.webp',
     linkUrl: 'https://heroic-lamington-f2f9a1.netlify.app/'
   }
