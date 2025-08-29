65b3c5b46e2e3e8d684b1178e4213d0e5fd4080f
arinleviti <arin.leviti@gmail.com>
2025-08-29 15:14:32 +0200

    placed bg video on cloudify

--- client/src/app/blog-page/blog-page.component.ts
+++ client/src/app/blog-page/blog-page.component.ts
@@ -1,4 +1,90 @@
-import { Component,ElementRef,ViewChild, inject, OnInit, } from '@angular/core';
+import { Component, ElementRef, ViewChild, inject, OnInit, AfterViewInit } from '@angular/core';
+import { BlogService } from '../_services/blog.service';
+import { BlogPost } from '../models/blog-post.model';
+import { BlogPostComponent } from "../blog-post/blog-post.component";
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+
+@Component({
+  selector: 'app-blog-page',
+  standalone: true,
+  imports: [BlogPostComponent, PaginationModule],
+  templateUrl: './blog-page.component.html',
+  styleUrls: ['./blog-page.component.css']
+})
+export class BlogPageComponent implements OnInit, AfterViewInit {
+  @ViewChild('backgroundVideoElement', { static: true }) backgroundVideoElement!: ElementRef<HTMLDivElement>;
+
+  private blogService = inject(BlogService);
+  video!: HTMLVideoElement;
+
+  blogPosts: BlogPost[] = [];
+  isLoggedIn: boolean = false;
+
+  pageNumber: number = 1;
+  pageSize: number = 5;
+  totalItems: number = 0;
+
+  ngOnInit(): void {
+    this.isAdminLoggedIn();
+    this.loadPaginatedPosts();
+  }
+
+  loadPaginatedPosts() {
+    this.blogService.getPaginatedPosts(this.pageNumber, this.pageSize).subscribe({
+      next: (paginatedResult) => {
+        this.blogPosts = paginatedResult.items;
+        this.totalItems = paginatedResult.pagination.totalItems;
+      },
+      error: (err) => console.error('Error loading blog posts:', err)
+    });
+  }
+
+  onPageChange(page: number): void {
+    this.pageNumber = page;
+    this.loadPaginatedPosts();
+  }
+
+  isAdminLoggedIn(): boolean {
+    this.isLoggedIn = sessionStorage.getItem('editAuth') === 'true';
+    return this.isLoggedIn;
+  }
+
+  ngAfterViewInit(): void {
+    this.createBackgroundVideo();
+  }
+
+  private createBackgroundVideo(): void {
+    this.video = document.createElement('video');
+    this.video.src = 'https://res.cloudinary.com/doydy0awd/video/upload/f_auto,q_auto/v1756472201/bgvideo_nrt3ps.webm';
+    this.video.autoplay = true;
+    this.video.loop = true;
+    this.video.muted = true;
+    this.video.playsInline = true;
+    this.video.preload = 'auto';
+
+    Object.assign(this.video.style, {
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      width: '100%',
+      height: '100%',
+      objectFit: 'cover',
+      zIndex: '0'
+    });
+
+    this.backgroundVideoElement.nativeElement.appendChild(this.video);
+    this.video.play().catch(e => console.log('Autoplay prevented:', e));
+  }
+
+  playVideo(): void {
+    if (!this.video) return;
+    if (this.video.paused) this.video.play().catch(() => {});
+    else this.video.pause();
+  }
+}
+
+
+/* import { Component,ElementRef,ViewChild, inject, OnInit, } from '@angular/core';
 import { BlogService } from '../_services/blog.service';
 import { BlogPost } from '../models/blog-post.model';
 import { BlogPostComponent } from "../blog-post/blog-post.component";
@@ -24,7 +110,7 @@ export class BlogPageComponent  {
   totalItems: number = 0; // Total number of items for pagination
 
   ngOnInit(): void {
-    /* this.loadPosts(); */
+    
     this.isAdminLoggedIn();
     this.loadPaginatedPosts();
   }
@@ -44,7 +130,7 @@ export class BlogPageComponent  {
     this.pageNumber = page;
     this.loadPaginatedPosts(); // Reload posts with the updated page number
   }
-  /* not used anymore */
+  
   loadPosts() {
     this.blogService.getBlogPosts().subscribe({
       next: (blogPosts: BlogPost[]) => {
@@ -66,7 +152,7 @@ export class BlogPageComponent  {
     return this.isLoggedIn;
   }
    ngAfterViewInit() {
-  const url = '/bgvideo.webm';
+  const url = 'https://res.cloudinary.com/doydy0awd/video/upload/f_auto,q_auto/v1756472201/bgvideo_nrt3ps.webm';
   const video = this.assetLoader.getVideoElement(url);
   if (!video) return;
 
@@ -97,3 +183,4 @@ export class BlogPageComponent  {
   }
  }
 
+ */
--- client/src/app/hero/hero.component.ts
+++ client/src/app/hero/hero.component.ts
@@ -1,5 +1,73 @@
 import { AfterViewInit, Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { ProfileImageComponent } from '../profile-image/profile-image.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [ProfileImageComponent, TranslateModule],
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent implements AfterViewInit {
+  @ViewChild('backgroundVideoElement', { static: true }) backgroundVideoElement!: ElementRef<HTMLDivElement>;
+
+  private translate = inject(TranslateService);
+  video!: HTMLVideoElement;
+
+  constructor() {
+    const lang = localStorage.getItem('lang') || 'en';
+    this.translate.setDefaultLang('en');
+    this.translate.use(lang);
+  }
+
+  ngAfterViewInit() {
+    this.createBackgroundVideo();
+  }
+
+  private createBackgroundVideo() {
+    this.video = document.createElement('video');
+    this.video.src = 'https://res.cloudinary.com/doydy0awd/video/upload/f_auto,q_auto/v1756472201/bgvideo_nrt3ps.webm';
+    this.video.autoplay = true;
+    this.video.loop = true;
+    this.video.muted = true;
+    this.video.playsInline = true;
+    this.video.preload = 'auto';
+
+    Object.assign(this.video.style, {
+      position: 'absolute',
+      zIndex: '-1',
+      top: '0',
+      left: '50%',
+      translate: '-50%',
+      width: '100%',
+      minHeight: '100%',
+      objectFit: 'cover'
+    });
+
+    this.backgroundVideoElement.nativeElement.appendChild(this.video);
+    this.video.play().catch(e => console.log('Autoplay prevented:', e));
+  }
+
+  playVideo() {
+    if (!this.video) return;
+
+    if (this.video.paused) {
+      this.video.play().catch(e => console.log('Play prevented:', e));
+    } else {
+      this.video.pause();
+    }
+  }
+
+  changeLang(lang: string) {
+    this.translate.use(lang);
+    localStorage.setItem('lang', lang);
+  }
+}
+
+
+/* import { AfterViewInit, Component, ElementRef, inject, ViewChild } from '@angular/core';
+import { ProfileImageComponent } from '../profile-image/profile-image.component';
 import { AssetLoaderService } from '../_services/asset-loader.service';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
@@ -29,7 +97,7 @@ export class HeroComponent implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    const url = '/bgvideo.webm';
+    const url = 'https://res.cloudinary.com/doydy0awd/video/upload/f_auto,q_auto/v1756472201/bgvideo_nrt3ps.webm';
     const video = this.assetLoader.getVideoElement(url);
     if (!video) return;
 
@@ -58,7 +126,7 @@ export class HeroComponent implements AfterViewInit {
     else this.video.pause();
   }
 }
-
+ */
  
 
 
